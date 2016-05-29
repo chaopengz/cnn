76e0befb8ecce74d3d164e3d63933f4bf6a91025
chaopengz <chaopengz@163.com>
2016-05-29 11:17:25 +0800

    add test.txt

--- app/views.py
+++ app/views.py
@@ -8,12 +8,13 @@ import flower
 data = 0
 predicted_class = 0
 
+
 def upload(request):
-    if request.method == 'POST':   
+    if request.method == 'POST':
         filename = request.FILES['image'].name
-       # imagePath = '/home/ubuntu/flower/media/uploads/' + str(int(time.time() * 1000)) + "-" + filename
-        imagePath='/home/ubuntu/flower/media/uploads/'+filename
-	print 'imagePath is ',imagePath
+        # imagePath = '/home/ubuntu/flower/media/uploads/' + str(int(time.time() * 1000)) + "-" + filename
+        imagePath = '/home/ubuntu/flower/media/uploads/' + filename
+        print 'imagePath is ', imagePath
         destination = open(imagePath, 'wb+')
         for chunk in request.FILES['image'].chunks():
             destination.write(chunk)
@@ -24,7 +25,7 @@ def upload(request):
         model_str = '/home/ubuntu/flower/app/model/model.json'
         model_weights = '/home/ubuntu/flower/app/model/weights_6.h5'
         global data, predicted_class
-        data, predicted_class = flower.model_predict(imagePath, model_str, model_weights,nb_classes, width)
+        data, predicted_class = flower.model_predict(imagePath, model_str, model_weights, nb_classes, width)
 
         return HttpResponse(predicted_class)
 
@@ -37,15 +38,16 @@ def index(request):
 
 
 def getResult(request):
-    global data ,predicted_class
+    global data, predicted_class
     width = 50
     nb_classes = 6
     model_str = 'model/model.json'
     model_weights = 'model/weights_6.h5'
     if request.method == 'POST':
         result = request.POST['result']
-
-        flower.feedback_train(data, predicted_class, result, model_str, model_weights, nb_classes, width)
-        return HttpResponse("Get your result.")
+        type = request.POST['type']
+        filename = request.POST['filename']
+        # flower.feedback_train(data, predicted_class, result, model_str, model_weights, nb_classes, width)
+        return HttpResponse(result + " " + type + " " + filename)
     else:
         return HttpResponse("No Post!")
